2637571871d8b784ad96356898cb910287948237
Aureo-Bueno <aureoalexandre@outlook.com>
2023-01-12 10:49:55 -0300

    remove /dist

--- dist/course/course.controller.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CourseController {
-    findAll(response: any): any;
-    findById(id: string): string;
-    create(body: any): any;
-    update(id: string, body: any): string;
-    remove(id: string): string;
-}
